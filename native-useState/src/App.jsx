5c13ab0c9d9348cf19daacf8fb4bbccb49c3e11b
B.M.N.D.S.Ariyarathna <88665386+SandalikaAriyrathna@users.noreply.github.com>
2026-07-16 01:14:02 +0530

    useState

---
+++ native-useState/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import './App.css'
+
+function NameList() {
+  const [list, setList] = useState(['John', 'Jane', 'Jack']);
+  const [name, setName] = useState('');
+
+  function addName() {
+    setList([...list, name]);
+    setName('');
+  }
+
+  return <div>
+    <ul>
+      {list.map((name, index) => <li key={index}>{name}</li>)}
+    </ul>
+
+    <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+    <button onClick={addName}>Add Name</button>
+
+  </div>
+
+}
+
+
+function Counter() {
+  const [count, setCount] = useState(10);
+
+  function addOne() {
+    setCount(count + 1);
+  }
+
+  return <div className='App'>
+
+    <button
+
+      onClick={addOne}
+
+    >Count = {count}</button>
+
+  </div>
+
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+      <NameList />
+
+
+    </div>
+  )
+}
+
+export default App
